efb646708667a0ac70a22837cb834617d8c1600d
Ana Carolina <anachan.01h@gmail.com>
2024-05-19 05:27:04 -0300

    Build[the-c-programming-language]: exercise 1-15

---
+++ the-c-programming-language/1-introduction/ex15.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+float f2c(float fahr);
+
+int main(void) {
+     float fahr;
+     int lower, upper, step;
+
+     lower = 0;
+     upper = 300;
+     step = 20;
+
+     fahr = lower;
+     while (fahr <= upper) {
+          printf("%3.0f %6.1f\n", fahr, f2c(fahr));
+          fahr = fahr + step;
+     }
+}
+
+float f2c(float fahr) {
+     return (5.0 / 9.0) * (fahr - 32.0);
+}
